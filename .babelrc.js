53ab6b8fec0a6a3ebe507cdd9cb17ede92c9bdce
B3nnyL <shuyinglin47@gmail.com>
2019-06-11 13:27:09 +1000

    add chatlio

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,7 @@ const env = {
   INTERCOM_ID: process.env.INTERCOM_ID || null,
   DRIFT_ID: process.env.DRIFT_ID || null,
   ZENDESK_ID: process.env.ZENDESK_ID || null,
+  CHATLIO_ID: process.env.CHATLIO_ID || null,
 }
 
 module.exports = {
