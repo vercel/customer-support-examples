48b41b54e69a3106ab15fb8a6ae6a7c2c1f05b51
B3nnyL <shuyinglin47@gmail.com>
2019-06-11 14:33:20 +1000

    add crisp

---
+++ components/crisp-logo.js
@@ -0,0 +1,18 @@
+const CrispLogo = () => (
+  <svg
+    width="95"
+    height="75"
+    viewBox="0 0 116 94"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M45.3 74.5L63.6 94l14.2-23 37.8-4.1L109.5.5.4 12.5l6 66.3 39-4.3z"
+      fill="#000"
+    />
+  </svg>
+)
+
+export default CrispLogo
